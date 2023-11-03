61616bfe378deaea246af4d095082afeaf3d8d19
wjdgnl81 <wjdgnl81@gmail.com>
2023-11-03 10:34:58 +0900

    Update 다음에 올 숫자.java

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -5,9 +5,11 @@ class Solution {
         int check = 0;
         int sequence = common[1]-common[0];
         int ratio = 0;
+        
         if (common[0]!=0){
          ratio = common[1]/common[0];
         }
+        
         if(common[2]-common[1]==sequence){
             check=1;
         }else if(common[2]/common[1]==ratio){
