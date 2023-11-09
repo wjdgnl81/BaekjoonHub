9f4755fcaa345e5bbf325d54def616d50e01894b
wjdgnl81 <wjdgnl81@gmail.com>
2023-11-09 17:59:38 +0900

    [level unrated] Title: 조건 문자열, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,30 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+
+        if(ineq===">"&& eq==="=")
+            if(n>=m){
+                answer=1
+            }else{
+                answer=0
+            }
+        if(ineq==="<"&& eq==="=")
+            if(n<=m){
+                answer=1
+            }else{
+                answer=0
+            }
+        if(ineq===">"&& eq==="!")
+            if(n>m){
+                answer=1
+            }else{
+                answer=0
+            }
+        if(ineq==="<" && eq==="!")
+            if(n<m){
+                answer=1
+            }else{
+                answer=0
+            }
+
+    return answer;
+}
